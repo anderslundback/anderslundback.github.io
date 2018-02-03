7b30f82202da9d951e0365ae85598862b11ab509
Anders <lundback.anders@gmail.com>
2018-02-03 17:53:17 +0000

    -reactbootstrap +simple menu

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+require("react-bootstrap/lib/NavbarHeader")
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <nav className="navbar-light">
+            <button className="btn btn-primary">
+              Home
+            </button>
+            <button className="btn btn-primary">
+              About
+            </button>
+            <button className="btn btn-primary">
+              Photos
+            </button>
+          </nav>
           <h1 className="App-title">Anders Lundbäck's personal website</h1>
         </header>
-        <p className="App-intro">
-          Coming soon&trade;
-        </p>
       </div>
     );
   }
